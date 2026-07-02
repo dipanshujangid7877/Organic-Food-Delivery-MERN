648aaa51e5ebdf8d65bef6b121a3a0640eb16462
dipasnhujangid7877 <dipanshujangid87@gmail.com>
2026-07-02 23:50:38 +0530

    Update login page UI

--- Foody-Shop/src/Components/Login.jsx
+++ Foody-Shop/src/Components/Login.jsx
@@ -1,9 +1,43 @@
- function Login() {
+import "../Styles/Login.css";
+import hero from "../assets/hero.jpeg";
+
+const Login = () => {
   return (
-    <div>
-      Login
+    <div className="login-container">
+      <div className="login-image">
+        <img src={hero} alt="Login" />
+      </div>
+
+      <div className="login-form">
+        <h1>Login</h1>
+        <p>Welcome Back</p>
+
+        <form>
+          <div className="input-group">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <div className="input-group">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <div className="Submit-group">
+            <button className="sign-in-button">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
